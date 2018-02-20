86c62832cd7a7379aa0cd3ec8d8a0df1697c5163
Mythili Sivakumar <mythili895@gmail.com>
2018-02-19 17:44:43 -0700

    Initial commit

---
+++ prediction.py
@@ -0,0 +1,23 @@
+import sklearn as skl
+from sklearn.ensemble import RandomForestClassifier
+import pandas as pd
+import numpy as np
+import csv
+from numpy import genfromtxt
+
+#setting random seed
+np.random.seed(0)
+
+#train_file = 'C:\Users\Mythili Sivakumar\Desktop\kaggle\titanic\dataset\train.csv'
+string1 = "C:/Users/Mythili Sivakumar/Desktop"
+string2 = "/kaggle/titanic/dataset/train.csv"
+#train_data = genfromtxt(string1+string2,delimiter = ',')
+train_file = string1+string2
+with open(train_file , 'r') as csvfile:
+    csvreader = csv.reader(csvfile)
+    for row in csvreader:
+        #print (row[0])
+        for col in row:
+            
+            if col in (None,""):
+                print(col)
